c0071a53ccbf37a25934b7712b607f60ec123ae1
ktony <k.tony9412@gmail.com>
2023-10-02 20:02:50 +0900

    UserLoanHistory의 isReturned 필드 ENUM으로 변경
    확장성을 고려하여 ENUM 타입을 사용하여 리팩토링
    fixture 적극 활용

--- src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
@@ -5,18 +5,35 @@ import javax.persistence.*
 
 @Entity
 class UserLoanHistory (
-    @ManyToOne
+        @ManyToOne
     var user: User,
 
-    val bookName: String,
-    var isReturn: Boolean,
+        val bookName: String,
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+        var status: UserLoanStatus = UserLoanStatus.LOANED,
+
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null
 ){
 
     fun doReturn() {
-        this.isReturn = true
+        this.status = UserLoanStatus.RETURNED
+    }
+
+    companion object {
+        fun fixture(
+            user: User,
+            bookName: String = "이상한 나라의 앨리스",
+            status: UserLoanStatus = UserLoanStatus.LOANED,
+            id: Long? = null,
+        ): UserLoanHistory {
+            return UserLoanHistory(
+                user = user,
+                bookName = bookName,
+                status = status,
+                id = id
+            )
+        }
     }
 
 }
--- src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -7,6 +7,7 @@ import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory
 import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository
+import com.group.libraryapp.domain.user.loanhistory.UserLoanStatus
 import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
@@ -65,7 +66,7 @@ class BookServiceTest @Autowired constructor(
         assertThat(results).hasSize(1)
         assertThat(results[0].bookName).isEqualTo("이상한 나라의 엘리스")
         assertThat(results[0].user.id).isEqualTo(savedUser.id)
-        assertThat(results[0].isReturn).isFalse()
+        assertThat(results[0].status).isEqualTo(UserLoanStatus.LOANED)
     }
 
     @DisplayName("책이 이미 대출되어 있다면, 신규 대출이 실패한다")
@@ -74,7 +75,7 @@ class BookServiceTest @Autowired constructor(
         // given
         bookRepository.save(Book.fixture("이상한 나라의 엘리스"))
         val savedUser = userRepository.save(User("A", null))
-        userLoanHistoryRepository.save(UserLoanHistory(savedUser, "이상한 나라의 엘리스", false))
+        userLoanHistoryRepository.save(UserLoanHistory.fixture(savedUser, "이상한 나라의 엘리스", UserLoanStatus.LOANED))
 
         val request = BookLoanRequest("A", "이상한 나라의 엘리스")
 
@@ -92,7 +93,7 @@ class BookServiceTest @Autowired constructor(
         // given
         bookRepository.save(Book.fixture("이상한 나라의 엘리스"))
         val savedUser = userRepository.save(User("A", null))
-        val loanHistory = userLoanHistoryRepository.save(UserLoanHistory(savedUser, "이상한 나라의 엘리스", false))
+        val loanHistory = userLoanHistoryRepository.save(UserLoanHistory.fixture(savedUser, "이상한 나라의 엘리스", UserLoanStatus.LOANED))
         val request = BookReturnRequest("A", "이상한 나라의 엘리스")
 
         // when
@@ -101,7 +102,7 @@ class BookServiceTest @Autowired constructor(
         // then
         val results = userLoanHistoryRepository.findAll()
         assertThat(results).hasSize(1)
-        assertThat(results[0].isReturn).isTrue()
+        assertThat(results[0].status).isEqualTo(UserLoanStatus.RETURNED)
 
     }
 
