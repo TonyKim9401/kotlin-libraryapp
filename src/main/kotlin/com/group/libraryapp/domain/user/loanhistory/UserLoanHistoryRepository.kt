81b387d684f77f59c1154fcbf36748917d06af59
ktony <k.tony9412@gmail.com>
2023-10-02 22:48:47 +0900

    서버에서 통계 메서드 작성 -> 건수가 올라갈수록 메모리의 부하가 심해질 가능성이 높다 DB에 통계 쿼리를 보내서 결괏값만 쏙 받는걸 추천!

--- src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
@@ -5,6 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository
 interface UserLoanHistoryRepository: JpaRepository<UserLoanHistory, Long> {
 
     fun findByBookNameAndStatus(bookName: String, status: UserLoanStatus): UserLoanHistory?
-    fun findAllByStatus(loaned: UserLoanStatus): List<UserLoanHistory>
+
+    fun findAllByStatus(status: UserLoanStatus): List<UserLoanHistory>
+
+    fun countByStatus(status: UserLoanStatus): Long
+
 
 }
