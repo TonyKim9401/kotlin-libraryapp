10c165a5cf8922aa5d8c31ec17e94a83cb2a9c64
ktony <k.tony9412@gmail.com>
2023-10-02 18:09:27 +0900

    Kotlin에서 클래스, 메서드 auto public open 사용을 위해 plugin 추가
    Java UserService -> Kotlin UserService로 리팩토링

---
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -0,0 +1,46 @@
+package com.group.libraryapp.service.user
+
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
+import com.group.libraryapp.dto.user.response.UserResponse
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class UserService (
+    private val userRepository: UserRepository
+){
+
+    @Transactional
+    fun saveUser(request: UserCreateRequest) {
+        /**
+         * 자바와는 다르게 default parameter 가 적용 된다
+         */
+        val newUser = User(request.name, request.age)
+        userRepository.save(newUser)
+    }
+
+    @Transactional(readOnly = true)
+    fun getUsers(): List<UserResponse> {
+        return userRepository.findAll()
+//                .map { user -> UserRepository(user) } // 평범한 문법
+//                .map { UserResponse(it) } // Kotlin 문법으로 같은 param 사용시 it으로 대체
+                .map(::UserResponse) // 각 객체를 바로 생성자에 넣어주기
+    }
+
+    @Transactional
+    fun updateUserName(request: UserUpdateRequest) {
+        val user = userRepository.findById(request.id).orElseThrow(::IllegalArgumentException) // IllegalArgumentException::new
+        user.updateName(request.name)
+    }
+
+    @Transactional
+    fun deleteUser(name: String) {
+        val user = userRepository.findByName(name).orElseThrow(::IllegalArgumentException) // IllegalArgumentException::new
+        userRepository.delete(user)
+    }
+
+}
