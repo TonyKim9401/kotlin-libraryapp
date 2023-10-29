11157496956514fec36cfa5220b34a51110feeee
ktony <k.tony9412@gmail.com>
2023-10-29 14:44:13 +0900

    commit

--- src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
+++ src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
@@ -17,11 +17,11 @@ import org.springframework.transaction.annotation.Transactional
 
 @Service
 class BookService(
-        private val bookRepository: BookRepository,
-        private val userRepository: UserRepository,
+    private val bookRepository: BookRepository,
+    private val userRepository: UserRepository,
 //        private val userLoanHistoryRepository: UserLoanHistoryRepository,
-        private val bookQuerydslRepository: BookQuerydslRepository,
-        private val userLoanHistoryQuerydslRepository: UserLoanHistoryQuerydslRepository
+    private val bookQuerydslRepository: BookQuerydslRepository,
+    private val userLoanHistoryQuerydslRepository: UserLoanHistoryQuerydslRepository
 ) {
 
     @Transactional
