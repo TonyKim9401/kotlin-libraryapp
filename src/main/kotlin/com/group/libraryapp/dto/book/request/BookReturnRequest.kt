c9e696830639eb2258adc20220f01e8cd864ffbb
ktony <k.tony9412@gmail.com>
2023-11-02 01:46:36 +0900

    commit

--- src/main/kotlin/com/group/libraryapp/dto/book/request/BookReturnRequest.kt
+++ src/main/kotlin/com/group/libraryapp/dto/book/request/BookReturnRequest.kt
@@ -1,6 +1,6 @@
 package com.group.libraryapp.dto.book.request
 
 data class BookReturnRequest(
-        val userName: String,
-        val bookName: String
+    val userName: String,
+    val bookName: String
 )
