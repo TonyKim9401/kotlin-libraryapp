f525c7486371fca7274bdae6c5d990995b3d47b2
ktony <k.tony9412@gmail.com>
2023-11-05 14:11:04 +0900

    commit

--- src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
+++ src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
@@ -3,7 +3,7 @@ package com.group.libraryapp.calculator
 import java.lang.IllegalArgumentException
 
 class Calculator (
-        var number: Int
+    var number: Int
 ){
     fun add(operand: Int){
         this.number += operand
