792cd03f5ec7b4f523136ea586f5c20a736df570
ktony <k.tony9412@gmail.com>
2023-11-08 10:15:31 +0900

    commit

--- src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
+++ src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
@@ -14,7 +14,6 @@ class Calculator (
     fun multiply(operand: Int) {
         this.number *= operand
     }
-
     fun divide(operand: Int) {
         if (operand == 0) {
             throw IllegalArgumentException("0으로 나눌 수 없습니다.")
