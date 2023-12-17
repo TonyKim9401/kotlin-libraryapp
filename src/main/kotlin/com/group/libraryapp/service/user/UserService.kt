cc3e0591e8ef16cc7d06f60c40eec6c7ec923075
ktony <k.tony9412@gmail.com>
2023-12-17 21:13:54 +0900

    commit

--- src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -43,7 +43,6 @@ class UserService (
         val user = userRepository.findByName(name) ?: fail() // IllegalArgumentException::new
         userRepository.delete(user)
     }
-
     @Transactional(readOnly = true)
     fun getUserLoanHistories(): List<UserLoanHistoryResponse> {
         return userRepository.findAllWithHistories()
